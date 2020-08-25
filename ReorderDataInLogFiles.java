4d7bf8f3da9ecf9f498ef9e7e096272fc8815e70
Ronn <jacob.r@husky.neu.edu>
2020-08-25 01:36:46 -0400

    Remove unnecessary comments for reorder data in log files problem

--- ReorderDataInLogFiles.java
+++ ReorderDataInLogFiles.java
@@ -1,18 +1,5 @@
 import java.util.*;
 /*
-You have an array of logs.  Each log is a space delimited string of words.
-
-For each log, the first word in each log is an alphanumeric identifier.  Then, either:
-
-Each word after the identifier will consist only of lowercase letters, or;
-Each word after the identifier will consist only of digits.
-We will call these two varieties of logs letter-logs and digit-logs.  It is guaranteed that each log has at 
-least one word after its identifier.
-
-Reorder the logs so that all of the letter-logs come before any digit-log.  The letter-logs are ordered lexicographically 
-ignoring identifier, with the identifier used in case of ties.  The digit-logs should be put in their original order.
-
-Return the final order of the logs.
 
 INT MAX VALUE CHECK NOTE !!!!!!!!!!!!!! Check for numbers not in the integer range.
  */
@@ -41,9 +28,9 @@ public class ReorderDataInLogFiles{
         Map<String, String> letterLogs = new TreeMap<>((a,b)-> a.compareTo(b));
         
         for(String log: logs){
-            int id_msg = log.indexOf(" ", 0);
-            String id = log.substring(0, id_msg);
-            String msg = log.substring(id_msg);
+            int idMsg = log.indexOf(" ", 0);
+            String id = log.substring(0, idMsg);
+            String msg = log.substring(idMsg);
             if(isDigitLog(log)){
                 digLogs.add(log);
             }
@@ -71,18 +58,9 @@ public class ReorderDataInLogFiles{
         System.out.println("937. Reorder Data in Log Files\n");
 
         // Leetcode Example.
-        // String[] logs1 = new String[] {"dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"};
+        String[] logs1 = new String[] {"dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"};
 
-        // Only letters
-        // String[] logs2 = new String[] {"let1 art can","let2 own kit dig","let3 art zero"};
-
-        // Only letters - tie.
-        String[] logs3 = new String[] {"let2 art zero","let3 own kit dig","let1 art zero"};
-
-        //Only digit
-        // String[] logs4 = new String[] {"dig2 3 6", "dig3 1 9","dig1 4 3"};
-
-        reorderLogFiles(logs3);
+        reorderLogFiles(logs1);
         System.out.println();
 
         
