7a490df79ca58e09efc6efb842c9ba4baf53b52d
Ronn <jacob.r@husky.neu.edu>
2020-08-29 02:23:52 -0400

    Correct implementation of Rotting Oranges

--- RottingOranges.java
+++ RottingOranges.java
@@ -12,6 +12,7 @@ public class RottingOranges {
     public static int orangesRotting(int[][] grid) {
         int result = 0;
         int nbOranges = 0;
+        boolean usesDiagonals = false;
         Queue<Orange> queue = new LinkedList();
         boolean[][] visited = new boolean[grid.length][grid[0].length];
         
@@ -40,61 +41,40 @@ public class RottingOranges {
                 result = current.time;
             }
             
-            if (checkValidRange(x, y - 1, grid.length, grid[0].length) && !visited[y - 1][x] && grid[y - 1][x] == 1) {
-                Orange north = new Orange();
-                north.x = x;
-                north.y = y - 1;
-                north.time = current.time + 1;
-                queue.offer(north);
-                visited[y - 1][x] = true;
-                grid[y - 1][x] = 2;
-            }
-            
-            if (checkValidRange(x, y + 1, grid.length, grid[0].length) && !visited[y + 1][x] && grid[y + 1][x] == 1) {
-                Orange south = new Orange();
-                south.x = x;
-                south.y = y + 1;
-                south.time = current.time + 1;
-                queue.offer(south);
-                visited[y + 1][x] = true;
-                grid[y + 1][x] = 2;
-            }
-            
-            if (checkValidRange(x - 1, y, grid.length, grid[0].length) && !visited[y][x - 1] && grid[y][x - 1] == 1) {
-                Orange west = new Orange();
-                west.x = x - 1;
-                west.y = y;
-                west.time = current.time + 1;
-                queue.offer(west);
-                visited[y][x - 1] = true;
-                grid[y][x - 1] = 2;
-            }
-            
-            if (checkValidRange(x + 1, y, grid.length, grid[0].length) && !visited[y][x + 1] && grid[y][x + 1] == 1) {
-                Orange east = new Orange();
-                east.x = x + 1;
-                east.y = y;
-                east.time = current.time + 1;
-                queue.offer(east);
-                visited[y][x + 1] = true;
-                grid[y][x + 1] = 2;
+            for (int i = y - 1; i <= y + 1; i++) {
+                for (int j = x - 1; j <= x + 1; j++) {
+                    if (!usesDiagonals && (y - i != 0 && x - j != 0)) continue;
+                    if (!isInBound(grid, j, i)) continue;
+                    if (visited[i][j]) continue;
+                    if (grid[i][j] != 1) continue;
+                    Orange rottingOrange = new Orange();
+                    rottingOrange.x = j;
+                    rottingOrange.y = i;
+                    rottingOrange.time = current.time + 1;
+                    visited[i][j] = true;
+                    grid[i][j] = 2;
+                    queue.offer(rottingOrange);
+                }
             }
         }
         
         return nbOranges > 0 ? -1 : result;
     }
-
-
-    public static boolean checkValidRange(int x, int y, int totRow, int totCol){
-        if(x < 0 || y < 0 || x >= totRow || y >= totCol){
-            return false;
-        }
+    
+    public static boolean isInBound(int[][] grid, int x, int y) {
+        if (y < 0 || y >= grid.length || x < 0 || x >= grid[0].length) return false;
         return true;
+    }
+    
+    public static class Orange {
+        public int x;
+        public int y;
+        public int time;
     }
 
     public static void main(String[] args){
         System.out.println("\n994. Rotting Oranges\n");
         int[][] grid = new int[][]{{2,1,1}, {1,1,0}, {0,1,1}};
-        System.out.println(initialize(grid));
+        System.out.println(orangesRotting(grid));
     }
 }
