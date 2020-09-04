d078dae59375e6336575034450a89b53f8838280
Ronn <jacob.r@husky.neu.edu>
2020-09-04 13:37:01 -0400

    1152. Analyze User Website Visit Pattern

--- AnalyzeUserWebsiteVisitPattern.java
+++ AnalyzeUserWebsiteVisitPattern.java
@@ -2,92 +2,60 @@ import java.util.*;
 
 public class AnalyzeUserWebsiteVisitPattern {
 
-    // Map<Username, TreeMap<TimeStamp, Website>> mp = new
-
-
-    public static List<String> getThreeSequences(Map<String, TreeMap<Integer, String>> visits){
-        List<String> visitResults = new ArrayList<>();
-        for(Map.Entry<String, TreeMap<Integer, String>> entry: visits.entrySet()){  
-            TreeMap<Integer, String> siteVisits = entry.getValue();
-            System.out.println(entry.getKey());
-            int k=0;
-            StringBuilder sb = new StringBuilder("");
-            for(Map.Entry<Integer, String> sites: siteVisits.entrySet()){
-                sb.append(sites.getValue()+"#");
-
-            }
-            visitResults.add(sb.toString());
-            sb = new StringBuilder("");
-        }
-        return visitResults;
-    }
-
-    public static String counter(List<String> visits){
-        TreeMap<String, Integer> mp = new TreeMap<>();
-        int maxLevel = 0;
-        String maxVisits = "";
-        for(String visit: visits){
-            System.out.println("\n" + visit );
-            String[] sities = visit.split("#");
-            int i=0; 
-            while(i+2<sities.length){
-                String s = sities[i] + "," +sities[i+1] + "," + sities[i+2];
-                System.out.println(s);
-                if(mp.containsKey(s)){
-                    int count = mp.get(s) + 1;
-                    if(maxLevel < count ){
-                        maxLevel = count;
-                        maxVisits = s;
-                    }
-                    else if( maxLevel == count){
-                        maxVisits =  s.compareTo(maxVisits) < 0  ? s : maxVisits;
-                    }
-                    mp.put(s, mp.get(s)+1);
-                }
-                else{
-                    if(maxLevel == 0){
-                        maxLevel = 1;
-                        maxVisits = s;
-                    }
-                    else if(maxLevel == 1){
-                        maxVisits =  s.compareTo(maxVisits) < 0  ? s : maxVisits;
-                    }
-                    mp.put(s, 1);
-                }
-                i++;
-            }
-        }
-        return maxVisits;
-    }
     public static List<String> mostVisitedPattern(String[] username, int[] timestamp, String[] website) {
-        int n = website.length;
-        if(username.length != n || timestamp.length != n){
-            return new ArrayList<>();
+        Integer[] indices = new Integer[timestamp.length];
+        for(int i=0; i<timestamp.length; i++){
+            indices[i] = i;
+        }
+        Arrays.sort(indices, (a,b)->timestamp[a]-timestamp[b]);
+
+        Map<String, ArrayList<String>> userSiteVisits = new HashMap<>();
+
+        for(int i=0; i<username.length; i++){
+            userSiteVisits.putIfAbsent(username[indices[i]], new ArrayList<>());
+            userSiteVisits.get(username[indices[i]]).add(website[indices[i]]);
         }
 
-        Map<String, TreeMap<Integer, String>> uNameVisitsMap = new HashMap<>();
-        for(int i=0; i<n; i++){
-            if(!uNameVisitsMap.containsKey(username[i])){
-                TreeMap<Integer, String> websiteVisits = new TreeMap<>((a,b)-> Integer.compare(a, b));
-                websiteVisits.put(timestamp[i], website[i]);
-                uNameVisitsMap.put(username[i], websiteVisits);
+        Map<String, Integer> tripletCount = new HashMap<>();
+
+        for(String user: userSiteVisits.keySet()){
+            ArrayList<String> sites = userSiteVisits.get(user);
+            Set<String> countedTriplets = new HashSet<>();
+            if(sites.size() < 3){
+                continue;
             }
-            else{
-                TreeMap<Integer, String> websiteVisits = uNameVisitsMap.get(username[i]);
-                websiteVisits.put(timestamp[i], website[i]);
-                uNameVisitsMap.put(username[i], websiteVisits);
+
+            for(int i=0; i<sites.size()-2; i++){
+                for(int j = i+1; j<sites.size()-1; j++){
+                    for(int k= j+1; k<sites.size(); k++){
+                        String triplet = sites.get(i) + "," + sites.get(j) + "," + sites.get(k);
+                        if(!countedTriplets.contains(triplet)){
+                            tripletCount.put(triplet, tripletCount.getOrDefault(triplet, 0)+1);
+                            countedTriplets.add(triplet);
+                        }
+                    }
+                }
             }
+
         }
 
-        String[] results = counter(getThreeSequences(uNameVisitsMap)).split(",");
-        return Arrays.asList(results);
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((a,b)->a.getValue()==b.getValue() ? a.getKey().compareTo(b.getKey())
+        : Integer.compare(b.getValue(), a.getValue()));
+
+        for(Map.Entry<String, Integer> entry: tripletCount.entrySet()){
+            pq.offer(entry);
+        }
+
+        return Arrays.asList(pq.poll().getKey().split(","));
+
     }
 
 
     public static void main(String[] args){
-        String[] username = new String[]{"zkiikgv","zkiikgv","zkiikgv","zkiikgv"};
-        int[] timestamp = new int[]{436363475,710406388,386655081,797150921};
-        String[] websites = new String[]{"wnaaxbfhxp","mryxsjc","oz","wlarkzzqht"};
-        mostVisitedPattern(username, timestamp, websites);
+        System.out.println("1152. Analyze User Website Visit Pattern");
+        String[] username = new String[]{"joe","joe","joe","james","james","james","james","mary","mary","mary"};
+        int[] timestamp = new int[]{1,2,3,4,5,6,7,8,9,10};
+        String[] websites = new String[]{"home","about","career","home","cart","maps","home","home","about","career"};
+        System.out.println(mostVisitedPattern(username, timestamp, websites));
     }
 }
