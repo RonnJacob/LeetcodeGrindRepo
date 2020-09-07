cd0cfd99432742f42bfa8e311de0014dce791751
Ronn <jacob.r@husky.neu.edu>
2020-09-06 22:16:06 -0400

    Add sorting: Selection Sort

--- SortingAlgorithms/Sorting.java
+++ SortingAlgorithms/Sorting.java
@@ -17,17 +17,39 @@ public class Sorting {
         return arr;
     }
 
+
+    public static int[] selectionSort(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            int temp = i;
+            for(int j=i; j<arr.length; j++){
+                if(arr[j] < arr[temp]){
+                   temp = j;
+                } 
+            }
+            if(temp != i){
+                int t = arr[i];
+                arr[i] = arr[temp];
+                arr[temp] = t;
+            }
+        }
+        return arr;
+    }
     
     
     public static void main(String[] args){
 
 
         int  arr[]={100,20,15,30,5,75,40};
-
-        
         System.out.println("\n Bubble Sort\n");
         System.out.println(Arrays.toString(arr));
         System.out.println(Arrays.toString(bubbleSort(arr)));
+
+
+        int  arr2[]={100,20,15,30,5,75,40};
+        System.out.println("\n Selection Sort\n");
+        System.out.println(Arrays.toString(arr2));
+        System.out.println(Arrays.toString(selectionSort(arr2)));
+
 
     }
     
