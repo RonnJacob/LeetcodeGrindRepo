bc34c0222fccc0d431d7f1288b50c6bc35073e22
Ronn <jacob.r@husky.neu.edu>
2020-09-10 13:43:23 -0400

    Add LRU Cache

--- LFUCache.java
+++ LFUCache.java
@@ -1,126 +1,140 @@
 import java.util.*;
-class Node{
-    int key; 
-    int val;
-    int count;
-    Node(int key, int val, int count){
-        this.key = key;
-        this.val = val;
-        this.count = count;
+public class LFUCache{
+
+     Map<Integer, Integer> keyValueStore;
+     Map<Integer, Integer> keyCounts;
+     Map<Integer, LinkedHashSet<Integer>> frequencyList;
+     int size;
+     int minFrequency;
+
+
+    // Constructor that initiates the maps for storing the necessary maps.
+    public LFUCache(int capacity){
+
+        // A map to store the key value stores.
+        keyValueStore = new HashMap<>();
+
+        // A map to store the counts of the all the keys.
+        keyCounts = new HashMap<>();
+
+        // A map to store the list of elements with a particular frequency.
+        frequencyList = new HashMap<>();
+
+        // The size of the cache.
+        size = capacity;
+
+        // The minimum frequency of an element.
+        minFrequency = -1;
+
+        frequencyList.put(1, new LinkedHashSet<>());
     }
 
-}
 
-public class LFUCache {
+    public void updateMin(int count){
+        if(minFrequency == count && frequencyList.get(count).isEmpty()){
+            minFrequency = count +1;
+        }
 
-    static int minCount;
-    static Map<Integer, Node> keyToNodeMapping;
-    static Map<Integer, LinkedList<Node>> freqNodes;
-    static int capacity = 3;
+        if(frequencyList.get(count).isEmpty()){
+            frequencyList.remove(count);
+        }
+    } 
 
-
-
-    public static int key(int key){
-        if(!keyToNodeMapping.containsKey(key)){
+    public int get(int key) {
+        if(size == 0){
             return -1;
         }
+        if(!keyValueStore.containsKey(key)){
+            return -1;
+        }
+        
+        int count = keyCounts.get(key);
+        frequencyList.get(count).remove(key);
+        updateMin(count);
+        if(!frequencyList.containsKey(count+1)){
+            frequencyList.put(count+1, new LinkedHashSet<>());
+        }
+        frequencyList.get(count+1).add(key);
+        keyCounts.put(key, count+1);
 
-        return keyToNodeMapping.get(key).val;
+        return keyValueStore.get(key);
     }
 
-    public static void put(int key, int value) {
-        if(keyToNodeMapping.size() + 1 > capacity){
-            removeLFU();
-        }
-        if(!keyToNodeMapping.containsKey(key)){
-            Node newNode = new Node(key, value, 0);
-            keyToNodeMapping.put(key, newNode);
-            minCount = 1;
-            update(key);
-        }
-        else{
-            Node newNode = keyToNodeMapping.get(key);
-            newNode.val = value;
-            keyToNodeMapping.put(key, newNode);
-            update(key);
-        }
-    }
-
-    public static void printlist(){
-        for(Map.Entry<Integer, LinkedList<Node>> entry: freqNodes.entrySet()){
-            System.out.print("Frequency " + entry.getKey() + " - ");
-            for(Node n: entry.getValue()){
-                System.out.print(n.key+","+n.val + "  ");
+    public void printList(){
+        System.out.println("\n===========Current Cache Values======");
+        for(Map.Entry<Integer, LinkedHashSet<Integer>> entry: frequencyList.entrySet()){
+            System.out.print(entry.getKey() + " : ");
+            for(Integer d: entry.getValue()){
+                System.out.print(d + "," + keyValueStore.get(d) + " ");
             }
             System.out.println();
         }
+        System.out.println();
 
+       
     }
+    
+    public void put(int key, int value) {
 
-    public static void removeLFU(){
-        LinkedList<Node> ns = freqNodes.get(minCount);
-        Node n = ns.remove(ns.size()-1);
-        keyToNodeMapping.remove(n.key);
-
-        if(ns.isEmpty()){
-            freqNodes.remove(minCount);
+        if(size == 0){
+            return;
         }
-        else{
-            freqNodes.put(minCount, ns);
-        }
+        
+        // If the key value store contains a key. Update the value and frequency. 
+        // Then update the frequency list.
+        if(keyValueStore.containsKey(key)){
+            keyValueStore.put(key, value);
 
-    }
+            // Get frequency of the key.
+            int count = keyCounts.get(key);
+            
+            // Update the keyCount with a new frequency.
+            keyCounts.put(key, count +1);
 
-    public static void update(int key){
+            // Remove the key from that frequency count and add it to the count+1 frequency.
+            frequencyList.get(count).remove(key);
 
-        Node nodeToUpdate = keyToNodeMapping.get(key);
-
-        if(nodeToUpdate.count == 0){
-            minCount = 1;
-            nodeToUpdate.count = nodeToUpdate.count +1;
-            if(!freqNodes.containsKey(nodeToUpdate.count)){
-                freqNodes.put(nodeToUpdate.count, new LinkedList<>());
-            }
-            freqNodes.get(nodeToUpdate.count).push(nodeToUpdate);
-        }
-        else{
-            LinkedList<Node> frequencyNodes = freqNodes.get(nodeToUpdate.count);
-            frequencyNodes.remove(nodeToUpdate);
-
-            if(frequencyNodes.isEmpty()){
-
-                freqNodes.remove(nodeToUpdate.count);
-
-                if(minCount == nodeToUpdate.count){
-                    minCount = nodeToUpdate.count+1;
-                }
+            if(!frequencyList.containsKey(count+1)){
+                frequencyList.put(count+1, new LinkedHashSet<>());
             }
             
-            nodeToUpdate.count = nodeToUpdate.count +1;
-            if(!freqNodes.containsKey(nodeToUpdate.count)){
-                freqNodes.put(nodeToUpdate.count, new LinkedList<>());
-            }
-            freqNodes.get(nodeToUpdate.count).push(nodeToUpdate);
+            frequencyList.get(count+1).add(key);
+            updateMin(count);
         }
-
+        else{
+            if(keyValueStore.size() + 1 > size){
+                
+                Object d =  frequencyList.get(minFrequency).toArray()[0];
+                keyValueStore.remove(d);
+                frequencyList.get(minFrequency).remove(d);
+                keyCounts.remove(d);
+            }
+            keyValueStore.put(key, value);
+            keyCounts.put(key, 1);
+            minFrequency = 1;
+            if(!frequencyList.containsKey(1)){
+                frequencyList.put(1, new LinkedHashSet<>());
+            }
+            frequencyList.get(1).add(key);
+        }
     }
 
 
-    
     public static void main(String[] args){
-        System.out.println("\n460. LFU Cache");
-        keyToNodeMapping = new HashMap<>();
-        freqNodes = new HashMap<>();
-        minCount = 1;
-        freqNodes.put(1, new LinkedList<>());
-    
-        put(1,1);
-        put(2,2);
-        put(3,3);
-        put(4,4);
-        put(5,5);
-        printlist();
+        System.out.println("\n LFU Cache");
+
+        LFUCache cache = new LFUCache(0);
+        cache.put(1,1);
+        cache.put(2,2);
+        cache.get(1);
+        cache.put(3,3);
+        System.out.println(" \nRetrieving 2 value from cache: " + cache.get(2));
+        System.out.println(" \nRetrieving 2 value from cache: " + cache.get(3));
+        cache.put(4,4);
+        System.out.println(" \nRetrieving 2 value from cache: " + cache.get(1));
+        System.out.println(" \nRetrieving 2 value from cache: " + cache.get(3));
+        System.out.println(" \nRetrieving 2 value from cache: " + cache.get(4));
+        cache.printList();
 
     }
-    
 }
