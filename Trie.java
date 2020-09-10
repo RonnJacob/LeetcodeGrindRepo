8c5b9a976ac109b16b50b496ee0609a10f90f149
Ronn <jacob.r@husky.neu.edu>
2020-09-10 18:07:14 -0400

    208. Implement Trie (Prefix Tree)

---
+++ Trie.java
@@ -0,0 +1,75 @@
+
+public class Trie {
+
+    class TrieNode{
+        public TrieNode[] children;
+        private boolean isEnd;
+    
+        public TrieNode(){
+            children = new TrieNode[26];
+        }
+    
+        public void setEndOfWord(){
+            isEnd = true;
+        }
+        public boolean isEnd(){
+            return isEnd;
+        }
+    
+    }
+
+    private TrieNode root;
+    public Trie(){
+        root= new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode parent = root;
+        for(Character ch: word.toCharArray()){
+            if(parent.children[ch-'a'] == null){
+                parent.children[ch-'a'] = new TrieNode();
+            }
+            parent = parent.children[ch-'a'];
+        }
+        parent.setEndOfWord();
+    }
+
+    public boolean search(String word){
+        TrieNode parent = root;
+        for(Character ch: word.toCharArray()){
+            if(parent.children[ch-'a'] == null){
+
+                System.out.println("NOT FOUND");
+                return false;
+            }
+            parent = parent.children[ch-'a'];
+        }
+        if(parent.isEnd()){
+            System.out.println("FOUND");
+            return true;
+        }
+
+        System.out.println("NOT FOUND");
+        return false;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode parent = root;
+        for(Character ch: prefix.toCharArray()){
+            if(parent.children[ch-'a'] == null){
+
+                return false;
+            }
+            parent = parent.children[ch-'a'];
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args){
+        System.out.println("\n208. Implement Trie (Prefix Tree)");
+        Trie tr = new Trie();
+
+    }
+    
+}
