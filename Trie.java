2a9569e2b200315ec4522d2904a0a85585353576
Ronn <jacob.r@husky.neu.edu>
2020-09-11 00:14:59 -0400

    Add 208. Implement Trie (Prefix Tree)

--- Trie.java
+++ Trie.java
@@ -1,10 +1,8 @@
-import java.util.*;
 public class Trie {
 
     class TrieNode{
         public TrieNode[] children;
         private boolean isEnd;
-        private String word = "";
     
         public TrieNode(){
             children = new TrieNode[26];
@@ -15,14 +13,6 @@ public class Trie {
         }
         public boolean isEnd(){
             return isEnd;
-        }
-
-        public void setWord(String word){
-            this.word = word;
-        }
-
-        public String getWord(){
-            return word;
         }
     
     }
@@ -41,7 +31,6 @@ public class Trie {
             parent = parent.children[ch-'a'];
         }
         parent.setEndOfWord();
-        parent.setWord(word);
     }
 
     public boolean search(String word){
@@ -63,74 +52,17 @@ public class Trie {
         return false;
     }
 
-    public List<String> findWords(char[][] board, String[] words) {
-
-        // Creates the tree for the word.
-        for(String word: words){
-            insert(word);
-        }
-
-
-        // Let's get to searching.
-
+    public boolean startsWith(String prefix) {
         TrieNode parent = root;
-        Set<String> result = new HashSet<>();
+        for(Character ch: prefix.toCharArray()){
+            if(parent.children[ch-'a'] == null){
 
-        for(int i=0; i<board.length; i++){
-            for(int j=0; j<board[0].length; j++){
-                if(parent.children[board[i][j]-'a'] != null){
-                    backtracking(i,j, parent, board, result);
-                }
+                return false;
             }
+            parent = parent.children[ch-'a'];
         }
 
-        System.out.println(result.toString());
-        return new ArrayList<>(result);
-    }
-
-    public void backtracking(int r, int c, TrieNode par, char[][] board, Set<String> result){
-
-        Character letter = board[r][c];
-        TrieNode parent = par.children[letter-'a'];
-
-        board[r][c] = '#';
-        
-        if(!parent.word.equals("")){
-            result.add(parent.word);
-        }
-
-
-        if(r-1 >=0 && board[r-1][c] != '#' && parent.children[board[r-1][c]-'a'] != null){
-            backtracking(r-1,c, parent, board, result);
-        }
-
-        if(r+1 < board.length && board[r+1][c] != '#' && parent.children[board[r+1][c]-'a'] != null){
-            backtracking(r+1,c, parent, board, result);
-        }
-
-        if(c-1 >=0 && board[r][c-1] != '#' && parent.children[board[r][c-1]-'a'] != null){
-            backtracking(r,c-1, parent, board, result);
-        }
-
-        if(c+1<board[0].length && board[r][c+1] != '#' && parent.children[board[r][c+1]-'a'] != null){
-            backtracking(r,c+1, parent, board, result);
-        }
-
-        board[r][c] = letter;
-
-
-    }
-
-   
-
-    public static void main(String[] args){
-        System.out.println("\n208. Implement Trie (Prefix Tree)");
-        Trie tr = new Trie();
-        String[] words = new String[]{"aba","baa","bab","aaab","aaa","aaaa","aaba"};
-        char[][] board = new char[][]{{'a','b'}, {'a', 'a'}};
-        tr.findWords(board, words);
-
-
+        return true;
     }
     
 }
