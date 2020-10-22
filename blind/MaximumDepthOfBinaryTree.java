3edc4833610288daac13b4f4e8ef12428d17e75e
Ronn <jacob.r@husky.neu.edu>
2020-10-22 13:39:32 -0400

    Add Maximum Depth of Binary Tree

--- blind/MaximumDepthOfBinaryTree.java
+++ blind/MaximumDepthOfBinaryTree.java
@@ -1,5 +1,6 @@
 package blind;
 
+import java.util.*;
 class TreeNode {
      int val;
      TreeNode left;
@@ -22,6 +23,21 @@ public class MaximumDepthOfBinaryTree {
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 
+    public static boolean isSameTree(TreeNode p, TreeNode q){
+
+       if(p == null && q == null){
+           return true;
+       }
+       if(p == null || q == null){
+           return false;
+       }
+
+       if(p.val != q.val){
+           return false;
+       }
+
+       return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
 
     public static void main(String[] args){
         System.out.println("\n104. Maximum Depth of Binary Tree");
@@ -30,7 +46,22 @@ public class MaximumDepthOfBinaryTree {
         root.right = new TreeNode(20);
         root.right.left = new TreeNode(15);
         root.right.right = new TreeNode(7);
-        System.out.println(maxDepth(root));
+
+        TreeNode root2 =new TreeNode(3);
+        root2.left = new TreeNode(9);
+        root2.right = new TreeNode(20);
+        root2.right.left = new TreeNode(15);
+        root2.right.right = new TreeNode(7);
+
+        TreeNode r1 = new TreeNode(1);
+        r1.left = new TreeNode(2);
+
+        TreeNode r2 = new TreeNode(1);
+        r2.right = new TreeNode(2);
+
+        System.out.println(isSameTree(root, root2));
+
+
     }
     
 }
